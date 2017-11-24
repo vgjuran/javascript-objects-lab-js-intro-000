e58b4afbe729afe3d7b52fbaad54f64d29f15f99
Vladimir <vgjuran@protonmail.com>
2017-11-24 17:21:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function destructivelyDeleteFromObjectByKey(object, key){
 
 function updateObjectWithKeyAndValue(object, key, value){
 
-  object[key] = new Object({ value })
+  object[key] = value
   return object
 }
